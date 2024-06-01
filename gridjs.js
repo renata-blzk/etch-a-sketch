e215c1f7bec63598fab3cbecf544c728d103d990
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-06-01 14:52:02 +0100

    add flex=wrap, change container and div sizes

--- gridjs.js
+++ gridjs.js
@@ -1,12 +1,12 @@
 let container = document.querySelector('.container');
 
 function createDivs() {
-    for (let i=0; i<16; i++) {
+    for (let i=0; i<256; i++) {
         let element = document.createElement('div');
         element.classList.add('div');
         container.appendChild(element);
     }
 }
 
-createDivs(16);
+createDivs(256);
 console.log(container);
