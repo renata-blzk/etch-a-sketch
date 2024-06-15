ffb575f4f45fb0156a185a006b15e27c06d24dd7
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-06-15 16:30:55 +0100

    include addEventListener within the loop

--- gridjs.js
+++ gridjs.js
@@ -4,29 +4,15 @@ function createDivs() {
     for (let i=0; i<256; i++) {
         let element = document.createElement('div');
         element.classList.add('div');
-        //element.onmouseover = changeColor;
-        container.appendChild(element);
-
-        let chngColor = document.querySelectorAll(':scope > .div');
-
-        chngColor.forEach(function(div) {
-            div.addEventListener('mouseover', function hover() {
-                element.style.backgroundColor='green';
-            })
+        
+        element.addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'black';
         });
+            
+        container.appendChild(element);
     
     }
+ }
 
-
-        /*element.addEventListener('mouseover', () => {
-            document.querySelectorAll('.div').style.backgroundColor="green";
-        });/*
-
-    }
-
-    /*function changeColor() {
-        document.querySelectorAll('.div').style.backgroundColor="green";
-    }*/
-}
 
 createDivs(256); 
