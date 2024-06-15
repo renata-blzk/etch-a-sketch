5f53920c93ac80197568ebf417f7aab0f7adbacc
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-06-15 18:36:27 +0100

    try to create grid of squares - divs appear only in columns

--- gridjs.js
+++ gridjs.js
@@ -1,8 +1,12 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
-function createDivs() {
-    for (let i=0; i<256; i++) {
+let row ;
+let column = row;
+let containerDivs = row * column;
+
+function createDivs(n) {
+    for (let i=0; i<n; i++) {
         let element = document.createElement('div');
         element.classList.add('div');
         
@@ -16,7 +20,7 @@ function createDivs() {
  }
 
 
-createDivs(256); 
+createDivs(4 * 4); 
 
 button.addEventListener('click', function() {
    const userInput = prompt ('Enter a number:');
