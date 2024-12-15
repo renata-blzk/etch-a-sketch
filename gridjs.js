857b2fbbb9077e63b1742f6ec2551edf72a84b9d
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-12-15 19:09:36 +0000

    follow yt tutorial for project, change html css & js

--- gridjs.js
+++ gridjs.js
@@ -1,12 +1,54 @@
-const container = document.querySelector('.container');
+const GRIDSIDE = 400;
+let rows = 16;
+let cols = 16;
+
+const sketchArea = document.querySelector("#sketch-area");
+sketchArea.style.width = `${GRIDSIDE}px`;
+sketchArea.style.height = `${GRIDSIDE}px`;
+
+function setBackgroundColor() {
+    this.style.backgroundColor = "black";
+}
+
+function createGridCells() {
+    for (let i = 0; i < (rows * cols); i++) {
+        const gridCell = document.createElement("div");
+
+        gridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
+        gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
+        gridCell.classList.add("cell");
+
+        sketchArea.appendChild(gridCell);
+
+        gridCell.addEventListener('mouseover', setBackgroundColor);
+    }
+};
+
+createGridCells();
+
+
+
+
+
+
+// ////////////////// MY CODE
+
+/*const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
-let row ;
-let column = row;
+let row;
+let column;
 let containerDivs = row * column;
 
-function createDivs(n) {
-    for (let i=0; i<n; i++) {
+
+//createDivs(4 * 4); 
+
+button.addEventListener('click', function() {
+   const userInput = prompt ('Enter a number:');
+   return userInput;
+
+   /*function createDivs(userInput) {
+    for (let i=0; i<userInput; i++) {
         let element = document.createElement('div');
         element.classList.add('div');
         
@@ -19,10 +61,35 @@ function createDivs(n) {
     }
  }
 
-
-createDivs(4 * 4); 
-
-button.addEventListener('click', function() {
-   const userInput = prompt ('Enter a number:');
-   return userInput;
  });
+
+slider.addEventListener('change', function() {
+    let boxes = document.querySelectorAll('.div');
+    boxes.forEach(box => {
+        box.remove();
+    });
+    createDivs();
+})
+
+ function createDivs() {
+    for (let i=0; i<(slider.value * slider.value); i++) {
+        let element = document.createElement('div');
+        let a = slider.value;
+        const boxSize = 530 / a;
+        grid.classList.add("grid");
+        grid.style.width = boxSize + "px";
+        grid.style.height = boxSize + "px";
+        element.classList.add('div');
+        
+        element.addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'black';
+        });
+            
+        container.appendChild(element);
+        container.appendChild(grid);
+    
+    }
+ }
+
+ createDivs(4); */
+ 
