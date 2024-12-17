69e666dc0c1863a0f6c914258fae052c1bd8bbee
renata-blzk <123456789+renata.brogaite@users.noreply.github.com>
2024-12-17 21:32:08 +0000

    add random color function to hover

--- gridjs.js
+++ gridjs.js
@@ -7,8 +7,13 @@ sketchArea.style.width = sketchArea.style.height = `${GRIDSIDE}px`;
 const enterbtn = document.querySelector('.enterbtn');
 
 function setBackgroundColor() {
-    this.style.backgroundColor = "black";
-}   
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
 
 function createGridCells(squaresPerSide) {
     sketchArea.replaceChildren();
@@ -23,7 +28,9 @@ function createGridCells(squaresPerSide) {
 
         sketchArea.appendChild(gridCell);
 
-        gridCell.addEventListener('mouseover', setBackgroundColor);
+        gridCell.addEventListener('mouseenter', () => {
+            gridCell.style.backgroundColor = setBackgroundColor();
+        });
     }
 }
 
